9ab397387715f951d38b98734f18ad6adac615ef
codalee123 <139011413+codalee123@users.noreply.github.com>
2024-08-29 17:32:20 -0500

    Add files via upload

--- EED.js
+++ EED.js
@@ -1,9 +1,9 @@
-const containerBackground = document.querySelector('.container-background')
+const bgImg= document.getElementById('bg-img')
 const change = ['/EEDImages/background cover.jpeg', '/EEDImages/chic handbag.jpeg','/EEDImages/Aside bag.jpeg','/EEDImages/fashionable hat.jpeg','/EEDImages/background cover.jpeg']
 let count = 0
 
 setInterval(() => {
-    containerBackground.style.backgroundImage = `url('${change[count]}')`
+    bgImg.src = change[count];
     if(count >3){
         count= 0
     }
